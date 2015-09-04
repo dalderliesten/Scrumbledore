b63cce8d3e94878d11d9ef26c70e2993b79851fc
David Alderliesten <j.w.d.alderliesten@student.tudelft.nl>
2015-09-04 16:33:04 +0200

    Remove unused import
    Removing some unused imports.
    
    > organization

--- src/main/java/nl/tudelft/scrumbledore/ScrumbledoreGUI.java
+++ src/main/java/nl/tudelft/scrumbledore/ScrumbledoreGUI.java
@@ -4,9 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
