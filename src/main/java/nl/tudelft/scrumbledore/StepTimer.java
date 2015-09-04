9d5a2e76ccf4b151c3ff564548f978d63c9f7038
Jesse Tilro (Laptop) <jesse@jessetilro.nl>
2015-09-04 22:37:08 +0200

    Add StepTimer class.
    Add StepTimer class, which will be responsible for periodically invoking
    the step method on a given game.

---
+++ src/main/java/nl/tudelft/scrumbledore/StepTimer.java
@@ -0,0 +1,12 @@
+package nl.tudelft.scrumbledore;
+
+/**
+ * Class responsible for invoking the step method on a Game class with a certain interval, in order
+ * to iterate through the cycles of a game.
+ * 
+ * @author Jesse Tilro
+ *
+ */
+public class StepTimer {
+
+}
