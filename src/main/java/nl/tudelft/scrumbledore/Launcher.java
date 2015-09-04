47ef37c0a9eb357b8b33b88ad2c78b0e02ccb150
Jesse Tilro (Laptop) <jesse@jessetilro.nl>
2015-09-04 22:39:18 +0200

    Add Launcher class.
    Add Launcher class which will connect all major component like Game,
    StepTimer and GUI, and will thus be responsible of setting up and
    launching a Game.

---
+++ src/main/java/nl/tudelft/scrumbledore/Launcher.java
@@ -0,0 +1,12 @@
+package nl.tudelft.scrumbledore;
+
+/**
+ * Class responsible for setting up and launching a game, and connecting it to the StepTimer and the
+ * GUI.
+ * 
+ * @author Jesse Tilro
+ *
+ */
+public class Launcher {
+
+}
