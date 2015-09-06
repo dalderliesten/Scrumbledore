ddbe3b436dffb8d33fd8e6591ec9cd536498088e
Niels Warnars <nwarnars@student.tudelft.nl>
2015-09-06 14:47:32 +0200

    Fixed uninitialized Player object bug in Gravity ad Kinetics classes < bug

--- src/main/java/nl/tudelft/scrumbledore/Player.java
+++ src/main/java/nl/tudelft/scrumbledore/Player.java
@@ -33,6 +33,7 @@ public class Player extends LevelElement {
   public boolean equals(Object other) {
     if (other instanceof Player) {
       Player that = (Player) other;
+      
       return (this.getPosition().equals(that.getPosition()) && this.getSize().equals(that.getSize()));
     }
     
