5d4da7ce79f67330a79842355d7ff5a63c153563
Jesse Tilro (Desktop) <jesse@jessetilro.nl>
2015-09-06 17:20:25 +0200

    Add CollisionType enumeration.
    Add enumeration CollisionType  for defining the type of a Collision.

---
+++ src/main/java/nl/tudelft/scrumbledore/CollisionType.java
@@ -0,0 +1,12 @@
+package nl.tudelft.scrumbledore;
+
+/**
+ * Collision type, describing from which direction collider is colliding collidee if they are
+ * colliding at all.
+ * 
+ * @author Tilro
+ *
+ */
+public enum CollisionType {
+  FromTop, FromBottom, FromLeft, FromRight, None
+}
