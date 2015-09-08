c48fe709296f9593662226177a1350ec736cffec
Niels Warnars <nwarnars@student.tudelft.nl>
2015-09-09 00:56:55 +0200

    Added functionality to obtain the coords of the floor under an NPC

--- src/main/java/nl/tudelft/scrumbledore/NPC.java
+++ src/main/java/nl/tudelft/scrumbledore/NPC.java
@@ -1,5 +1,7 @@
 package nl.tudelft.scrumbledore;
 
+import java.util.ArrayList;
+
 /**
  * Class representing an NPC in a game.
  * 
@@ -24,6 +26,71 @@ public class NPC extends LevelElement {
     setGravity(true);
   }
 
+  
+  /**
+   * Helper method used to determine the position of 
+   * a Platform in an ArrayList<Platform> based on
+   * a given position.
+   * 
+   * @param platforms
+   *          An ArrayList of Platforms
+   * @param pos
+   *          The position of a certain Platform
+   * @return
+   *          The index of a certain Platform in an
+   *          ArrayList
+   */
+  protected static int getIndexFromPos(ArrayList<Platform> platforms, Vector pos) {
+    for (int i = 0; i < platforms.size(); i++) {
+      if (pos.equals(platforms.get(i).getPosition())) {
+        return i;
+      }
+    }
+    
+    return -1;
+  }
+  
+  
+  /**
+   * 
+   * @param platforms
+   *          An ArrayList of Platforms
+   * @return
+   *          A Vector array with the left and right
+   *          boundaries of the underlaying floor
+   */
+  protected Vector[] getUnderlayingFloorBoundaries(ArrayList<Platform> platforms) {
+    Vector[] floorBoundaries = new Vector[2]; 
+    Vector enemyPos = getPosition();
+    Vector pUnderEnemyPos = Vector.sum(enemyPos, new Vector(0, Constants.BLOCKSIZE));
+    
+    // Get left most platform boundary
+    for (int i = getIndexFromPos(platforms, pUnderEnemyPos); i > 0; i--) {
+      Vector currentPlatformPos = platforms.get(i).getPosition();
+      Vector previousPlatformPos = platforms.get(i-1).getPosition();
+      Vector expectedPreviousPlatformPos = Vector.difference(currentPlatformPos, new Vector(Constants.BLOCKSIZE, 0));
+
+      if (!previousPlatformPos.equals(expectedPreviousPlatformPos)) {
+        floorBoundaries[0] = currentPlatformPos;
+        break;
+      }
+    }
+    
+    // Get right most platform boundary
+    for (int i = getIndexFromPos(platforms, pUnderEnemyPos); i < platforms.size(); i++) {
+      Vector currentPlatformPos = platforms.get(i).getPosition();
+      Vector nextPlatformPos = platforms.get(i+1).getPosition();
+      Vector expectedNextPlatformPos = Vector.sum(currentPlatformPos, new Vector(Constants.BLOCKSIZE, 0));
+
+      if (!nextPlatformPos.equals(expectedNextPlatformPos)) {
+        floorBoundaries[1] = currentPlatformPos;
+        break;
+      }
+    }
+    
+    return floorBoundaries;
+  }
+  
   
   /**
    * Check whether a given object is equal to this instance.
--- src/test/java/nl/tudelft/scrumbledore/NPCTest.java
+++ src/test/java/nl/tudelft/scrumbledore/NPCTest.java
@@ -1,5 +1,10 @@
 package nl.tudelft.scrumbledore;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //import static org.junit.Assert.*;
 
 import org.junit.AfterClass;
@@ -38,4 +43,44 @@ public class NPCTest extends LevelElementTest {
     return new NPC(position, size);
   }
 
+  
+  /**
+   * Test case for the getIndexFromPos that checks whether
+   * the returned index matches the actual position of the
+   * intended Platform object.
+   */
+  @Test
+  public void testGetIndexFromPos() {
+    Level level = new Level();
+    String testMap = " N  \n" + " ## \n";
+
+    try {
+      level = (new LevelParser()).readLevelFromScanner(new Scanner(testMap));
+    } catch (FileNotFoundException e) { }
+    
+    Vector vt = new Vector(2 * Constants.BLOCKSIZE, Constants.BLOCKSIZE);
+    assertEquals(1, NPC.getIndexFromPos(level.getPlatforms(), vt));    
+  }
+  
+  /**
+   * Test case for the getUnderlayingPlatformsBoundaries method
+   * which checks whether the platform boundaries returned are
+   * the correct ones.
+   */
+  @Test
+  public void testGetUnderlayingFloorBoundaries() {
+    Level level = new Level();
+    String testMap = "   N    \n" + "# #### #\n";
+
+    try {
+      level = (new LevelParser()).readLevelFromScanner(new Scanner(testMap));
+    } catch (FileNotFoundException e) { }
+
+    NPC npc = (NPC) level.getMovingElements().get(0);
+    Vector[] boundaries = npc.getUnderlayingFloorBoundaries(level.getPlatforms());
+    
+    // Check left and right boundaries
+    assertEquals(new Vector(2 * Constants.BLOCKSIZE, Constants.BLOCKSIZE), boundaries[0]);  
+    assertEquals(new Vector(5 * Constants.BLOCKSIZE, Constants.BLOCKSIZE), boundaries[1]);    
+  }
 }
