19a12d15df94b3e5d49b99f856f499bd424e613d
Floris Doolaard <floris.do@gmail.com>
2015-09-08 12:50:47 +0200

    Updated collisions with player and fruit.

--- src/main/java/nl/tudelft/scrumbledore/CollisionsLevelModifier.java
+++ src/main/java/nl/tudelft/scrumbledore/CollisionsLevelModifier.java
@@ -11,13 +11,15 @@ import java.util.ArrayList;
 public class CollisionsLevelModifier implements LevelModifier {
 
   private KineticsLevelModifier kinetics;
+  private ScoreCounter score;
   
   /**
    * Constructs a new Collisions Level Modifier using a given Kinetics Level Modifier.
    * @param kinetics The Kinetics Level Modifier to be used.
    */
-  public CollisionsLevelModifier(KineticsLevelModifier kinetics) {
+  public CollisionsLevelModifier(KineticsLevelModifier kinetics, ScoreCounter score) {
     this.kinetics = kinetics;
+    this.score = score;
   }
   
   /**
@@ -31,16 +33,28 @@ public class CollisionsLevelModifier implements LevelModifier {
   public void modify(Level level, double delta) {
     detectPlayer(level, delta);
   }
+  
+  /**
+   * Executing all detecting methods with the player.
+   * @param level
+   *          The level.
+   * @param delta
+   *          The delta provided by the StepTimer.
+   */
+  public void detectPlayer(Level level, double delta) {
+    detectPlayerPlatform(level, delta);
+    detectPlayerFruit(level, delta);
+  }
 
   /**
-   * Detect collisions between player and other LevelElements.
+   * Detect collisions between player and platform.
    * 
    * @param level
    *          The Level.
    * @param delta
    *          The delta provided by the StepTimer.
    */
-  public void detectPlayer(Level level, double delta) {
+  public void detectPlayerPlatform(Level level, double delta) {
     Player player = level.getPlayer();
 
     // Find platform collidee candidates.
@@ -59,6 +73,37 @@ public class CollisionsLevelModifier implements LevelModifier {
       if (collision.collidingFromTop()) {
         kinetics.stopVertically(player);
         kinetics.snapTop(player, platform);
+      }
+    }
+  }
+  
+  /**
+   * Detect collisions between player and fruit element.
+   * 
+   * @param level
+   *          The Level.
+   * @param delta
+   *          The delta provided by the StepTimer.
+   */
+  public void detectPlayerFruit(Level level, double delta) {
+    Player player = level.getPlayer();
+
+    // Find platform collidee candidates.
+    ArrayList<Fruit> candidates = new ArrayList<Fruit>();
+
+    for (Fruit fruit : level.getFruits()) {
+      double dist = player.distance(fruit);
+      if (dist <= Constants.COLLISION_RADIUS) {
+        candidates.add(fruit);
+      }
+    }
+
+    // Detect collisions with candidates.
+    for (Fruit fruit : candidates) {
+      Collision collision = new Collision(player, fruit, delta);
+      if (collision.collidingFromTop()) {
+        score.updateScore(fruit.getValue());
+        // TODO DELETE FRUIT
       }
     }
   }
