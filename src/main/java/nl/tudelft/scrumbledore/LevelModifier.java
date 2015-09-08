89863adf4dcbb3febe391c2684b1462c46f90612
Jesse Tilro (Laptop) <jesse@jessetilro.nl>
2015-09-08 11:34:21 +0200

    Add LevelModifier interface.

---
+++ src/main/java/nl/tudelft/scrumbledore/LevelModifier.java
@@ -0,0 +1,10 @@
+package nl.tudelft.scrumbledore;
+
+/**
+ * A LevelModifier can be used by the Game to update the state of the Level and its LevelElements.
+ * @author Jeroen Meijer and Jesse Tilro
+ *
+ */
+public interface LevelModifier {
+
+}
