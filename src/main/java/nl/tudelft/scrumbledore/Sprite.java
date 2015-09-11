ae31b7517d54a061810802842d2f606ed9c57099
Jesse Tilro (Laptop) <jesse@jessetilro.nl>
2015-09-11 14:12:46 +0200

    Add Sprite class.
    Add Sprite class which can be used for the visual representation of a
    LevelElement.

---
+++ src/main/java/nl/tudelft/scrumbledore/Sprite.java
@@ -0,0 +1,58 @@
+package nl.tudelft.scrumbledore;
+
+import javafx.scene.image.Image;
+
+/**
+ * Represents a sprite that can be drawn in the GUI as a visual representation of a LevelElement.
+ * 
+ * @author Jesse Tilro
+ *
+ */
+public class Sprite {
+
+  private String id;
+  private String ext;
+
+  /**
+   * Constructs a new sprite instance with the given id to reference an image file.
+   * 
+   * @param id
+   *          Reference to an image file.
+   */
+  public Sprite(String id) {
+    this.id = id;
+    this.ext = "png";
+  }
+
+  /**
+   * Constructs a new sprite instance with the given id to reference an image file.
+   * 
+   * @param id
+   *          Reference to an image file.
+   * @param ext
+   *          The extension of the image file.
+   */
+  public Sprite(String id, String ext) {
+    this.id = id;
+    this.ext = ext;
+  }
+
+  /**
+   * The the path to the file to be used for this sprite.
+   * 
+   * @return File path.
+   */
+  public String getPath() {
+    return Constants.SPRITES_DIR + id + "." + ext;
+  }
+
+  /**
+   * Get a JavaFX Image instance for this sprite.
+   * 
+   * @return An Image instance.
+   */
+  public Image getImage() {
+    return new Image(getPath());
+  }
+
+}
