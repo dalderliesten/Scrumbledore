18d31190a832d7585842f728491f35f330d6d2e3
David Alderliesten <j.w.d.alderliesten@student.tudelft.nl>
2015-09-15 11:21:13 +0200

    Fix logger dir
    Fixed the logger folder, which did not create the folder in the correct
    source folder.
    
    > bug

--- src/main/java/nl/tudelft/scrumbledore/Logger.java
+++ src/main/java/nl/tudelft/scrumbledore/Logger.java
@@ -2,6 +2,7 @@ package nl.tudelft.scrumbledore;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Class for the maintainence and creation of logging files, which track movement and actions in
@@ -22,7 +23,13 @@ public class Logger {
    * needed writers and file functions.
    */
   public Logger() {
+    loggingDir = new File(Constants.RESOURCES_DIR + Constants.LOGGER_DIR);
 
+    // Checking to see if the directory for the logs exists. If this is no the case, it will be
+    // created.
+    if (loggingDir.exists() == false) {
+      loggingDir.mkdir();
+    }
   }
 
 }
