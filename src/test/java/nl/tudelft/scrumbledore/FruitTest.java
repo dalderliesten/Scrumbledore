e2be789e5ac50407a0f2a1728e2f4bd9cdc3b61a
Niels Warnars <nwarnars@student.tudelft.nl>
2015-09-17 20:53:17 +0200

    Adds tests for fruit::equals/hashcode methods < organisation

--- src/test/java/nl/tudelft/scrumbledore/FruitTest.java
+++ src/test/java/nl/tudelft/scrumbledore/FruitTest.java
@@ -2,6 +2,7 @@ package nl.tudelft.scrumbledore;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -34,6 +35,48 @@ public class FruitTest {
     
     fruit.setValue(42);
     assertEquals(42, fruit.getValue());
+  }
+  
+  /**
+   * Test dummy hashCode method.
+   */
+  @Test
+  public void testHashCode() {
+    Fruit fruit = new Fruit(new Vector(0, 0), new Vector(32, 32));
+    assertEquals(0, fruit.hashCode());
+ 
+  }
+  /**
+   * Test the equals method with two the same objects.
+   */
+  @Test
+  public void testEqualsTrue() {
+    Fruit fruit1 = new Fruit(new Vector(0, 0), new Vector(32, 32));
+    Fruit fruit2 = new Fruit(new Vector(0, 0), new Vector(32, 32));
+   
+    assertTrue(fruit1.equals(fruit2));
+  }
+  
+  /**
+   * Test the equals method with two different objects.
+   */
+  @Test
+  public void testEqualsFalse() {
+    Fruit fruit1 = new Fruit(new Vector(0, 0), new Vector(32, 32));
+    Fruit fruit2 = new Fruit(new Vector(1, 1), new Vector(32, 32));
+   
+    assertFalse(fruit1.equals(fruit2));
+  }
+  
+  /**
+   * Test the equals method with two different object classes.
+   */
+  @Test
+  public void testEqualsOtherObject() {
+    Fruit fruit = new Fruit(new Vector(0, 0), new Vector(32, 32));
+    NPC npc = new NPC(new Vector(0, 0), new Vector(32, 32));
+   
+    assertFalse(fruit.equals(npc));
   }
 
 }
