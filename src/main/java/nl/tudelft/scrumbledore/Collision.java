c1eafa8564b15976c7264f22ead881882a1fa6b8
Jesse Tilro (Desktop) <jesse@jessetilro.nl>
2015-10-06 21:27:44 +0200

    Fix collision touchingRight method.
    Fixed the Collision class touchingRight method: the Collision Precision
    constant was applied incorrectly, resulting in the player being able to
    move through walls from the right.
    
    <bug

--- src/main/java/nl/tudelft/scrumbledore/Collision.java
+++ src/main/java/nl/tudelft/scrumbledore/Collision.java
@@ -197,8 +197,8 @@ public class Collision {
    * @return Boolean.
    */
   private boolean touchingRight() {
-    boolean c1 = collider.getLeft() >= collidee.getRight();
-    boolean c2 = collider.getLeft() <= (collidee.getRight() - Constants.COLLISION_PRECISION);
+    boolean c1 = collider.getLeft() >= collidee.getRight() - Constants.COLLISION_PRECISION;
+    boolean c2 = collider.getLeft() <= collidee.getRight();
     return (colliding() && c1 && c2);
   }
 
