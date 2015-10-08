40166507e022a8d4f660d5d574768dfbf2e7dae6
Niels Warnars <nwarnars@student.tudelft.nl>
2015-10-08 21:16:38 +0200

    Add test suite for PlayerAction enum
    Adds several test cases covering all branches of the invertAction method
    <organisation

---
+++ src/test/java/nl/tudelft/scrumbledore/PlayerActionTest.java
@@ -0,0 +1,72 @@
+package nl.tudelft.scrumbledore;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Test suite for the Player Action enum.
+ * 
+ * @author Niels Warnars
+ */
+public class PlayerActionTest {
+  
+  /**
+   * Test the invertAction method with a given
+   * MoveRight action. 
+   * Expected outcome: MoveStop.
+   */
+  @Test
+  public final void testInvertActionMoveRight() {
+    PlayerAction result = 
+        PlayerAction.invertAction(PlayerAction.MoveRight);
+    assertEquals(PlayerAction.MoveStop, result);
+  }
+
+  /**
+   * Test the invertAction method with a given
+   * MoveLeft action.
+   * Expected outcome: MoveStop.
+   */
+  @Test
+  public final void testInvertActionMoveLeft() {
+    PlayerAction result = 
+        PlayerAction.invertAction(PlayerAction.MoveLeft);
+    assertEquals(PlayerAction.MoveStop, result);
+  }
+  
+  /**
+   * Test the invertAction method with a given
+   * Shoot action.
+   * Expected outcome: ShootStop.
+   */
+  @Test
+  public final void testInvertActionShoot() {
+    PlayerAction result = 
+        PlayerAction.invertAction(PlayerAction.Shoot);
+    assertEquals(PlayerAction.ShootStop, result);
+  }
+  
+  /**
+   * Test the invertAction method with an unsupported 
+   * action.
+   * Expected outcome: null.
+   */
+  @Test
+  public final void testInvertActionUnsupported() {
+    PlayerAction result = 
+        PlayerAction.invertAction(PlayerAction.Jump);
+    assertEquals(null, result);
+  }
+
+  /**
+   * Test the invertAction method with null given.
+   * Expected outcome: null.
+   */
+  @Test
+  public final void testInvertActionNull() {
+    PlayerAction result = 
+        PlayerAction.invertAction(null);
+    assertEquals(null, result);
+  }
+}
