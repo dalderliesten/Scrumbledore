e1e180aa0504e48c98dcd11663beedba99c6657c
Jeroen Meijer(Desktop) <J.Meijer-5@student.tudelft.nl>
2015-10-11 15:05:57 +0200

    Setup of skeleton class for KeybindingSettings.

---
+++ src/main/java/nl/tudelft/scrumbledore/userinterface/KeybindingSettings.java
@@ -0,0 +1,43 @@
+package nl.tudelft.scrumbledore.userinterface;
+
+import javafx.scene.layout.VBox;
+import nl.tudelft.scrumbledore.Constants;
+
+/**
+ * Class responsible for generating the keybinding options and the arming of all graphical elements for
+ * these options.
+ * 
+ * @author Jeroen Meijer
+ */
+public final class KeybindingSettings {
+
+  private static VBox currentBox;
+
+  /**
+   * Constructor is set to private, as only one instance of the settings menu should exist at any
+   * given time.
+   */
+  private KeybindingSettings() {
+
+  }
+
+  /**
+   * Performs all the actions required to get the settings for logging.
+   * 
+   * @return A JavaFX VBox containing all the options and associated actions.
+   */
+  public static VBox fetchLoggingOptions() {
+    currentBox = new VBox(Constants.SETTINGS_PADDING);
+
+    generateOptions();
+
+    return currentBox;
+  }
+
+  /**
+   * Generates the options needed for the keybindings in the settings menu.
+   */
+  private static void generateOptions() {
+    
+  }
+}
