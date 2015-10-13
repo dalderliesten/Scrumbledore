75b3750b847338e7eb297d15644f08a1f52ffe6c
Floris Doolaard <floris.do@gmail.com>
2015-10-13 11:50:10 +0200

    Fixed staircase and movement problem
    The player can now walk without any lag and the system is able to process multiple key presses.
    <bug

--- src/main/java/nl/tudelft/scrumbledore/level/PlayerAction.java
+++ src/main/java/nl/tudelft/scrumbledore/level/PlayerAction.java
@@ -16,15 +16,20 @@ public enum PlayerAction {
    *          Action to be inverted
    * @return inverted action
    */
-  public static PlayerAction invertAction(PlayerAction action) {
+  public static PlayerAction invertAction(Player player, PlayerAction action) {
     if (action != null) {
       switch (action) {
       case MoveRight:
+        player.removeAction(PlayerAction.MoveRight);
         return MoveStop;
       case MoveLeft:
+        player.removeAction(PlayerAction.MoveLeft);
         return MoveStop;
       case Shoot:
+        player.removeAction(PlayerAction.Shoot);
         return ShootStop;
+      case Jump:
+        player.removeAction(PlayerAction.Jump);
       default:
         return null;
       }
