c9a82d9d320907355510965e5645315982a99fc7
David Alderliesten <j.w.d.alderliesten@student.tudelft.nl>
2015-10-20 19:00:46 +0200

    Correct Keybind implement
    Correctly implemented the key-binding pop-up window and styled it.
    
    <feature

--- src/main/java/nl/tudelft/scrumbledore/Constants.java
+++ src/main/java/nl/tudelft/scrumbledore/Constants.java
@@ -92,7 +92,7 @@ public final class Constants {
   public static final String SETTINGSEXIT_BUTTON = "Back";
   public static final String SETTINGSLOGGING_BUTTON = "Logging";
   public static final String SETTINGSKEYBINDING_BUTTON = "Keybinding";
-  public static final String SETTINGS_CHOOSEKEYQUERY = "Press The desired key...";
+  public static final String SETTINGS_CHOOSEKEYQUERY = "Press the desired key...";
   public static final int SETTINGS_CHOOSEKEYPADDING = 50;
   
   // Defining the text needed for the display of the keybinding options.
