ab2fe4e32fe62a270071964ee304d16b21cc992a
Jesse Tilro (Desktop) <jesse@jessetilro.nl>
2015-10-21 17:40:39 +0200

    Fix Logger Test testLog method.
    The testLog method assumend the log file used for the test will no lines
    other than the ones written by the test method. However, when logging is
    enabled during the execution of preceding test cases, other lines may be
    written to the file used for testLog as well. Therefore, only the last
    line of the log should be compared to the one printed by the test case.
    
    <testing,bug

--- src/test/java/nl/tudelft/scrumbledore/LoggerTest.java
+++ src/test/java/nl/tudelft/scrumbledore/LoggerTest.java
@@ -2,15 +2,16 @@ package nl.tudelft.scrumbledore;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+
 import org.junit.Before;
 import org.junit.Test;
-
 
 /**
  * Test suite for the Logger class.
@@ -28,14 +29,14 @@ public class LoggerTest {
   public void setUp() {
     logger = Logger.getInstance();
   }
-  
+
   /**
    * Test whether only one instance of a Logger can exist.
    */
   @Test
   public final void testGetInstance() {
     Logger logger2 = Logger.getInstance();
-    
+
     assertEquals(logger, logger2);
   }
 
@@ -43,37 +44,41 @@ public class LoggerTest {
    * Test whether the correct logging directory has been created.
    */
   @Test
-  public final void testLoggingDir() {    
+  public final void testLoggingDir() {
     File loggingDir = new File(Constants.APPDATA_DIR + Constants.LOGGER_DIR);
     assertTrue(loggingDir.exists());
   }
-  
+
   /**
    * Test whether the correct logging file has been created.
    */
   @Test
   public final void testLoggingFile() {
     File loggingFile = logger.getLoggingFile();
-    
+
     assertTrue(loggingFile.exists());
   }
-  
+
   /**
-   * Test whether the desired data is written to disk when the Logger.log() method 
-   * is being called.
+   * Test whether the desired data is written to disk when the Logger.log() method is being called.
    */
   @Test
   public final void testLog() {
     File loggingFile = logger.getLoggingFile();
     logger.log("log_write_test");
-        
+
     try {
-      BufferedReader reader 
-        = new BufferedReader(new InputStreamReader(new FileInputStream(loggingFile), "UTF-8"));
-      
+      BufferedReader reader = new BufferedReader(
+          new InputStreamReader(new FileInputStream(loggingFile), "UTF-8"));
+
       assertEquals("--------------------SCRUMBLEDORE LOGGING FILE", reader.readLine());
-      assertEquals("log_write_test", reader.readLine());
-      
+      String lastLine = "";
+      String line;
+      while ((line = reader.readLine()) != null) {
+        lastLine = line;
+      }
+      assertEquals("log_write_test", lastLine);
+
       reader.close();
     } catch (UnsupportedEncodingException e) {
       e.printStackTrace();
