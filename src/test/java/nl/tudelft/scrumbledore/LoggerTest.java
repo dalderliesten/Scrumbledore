2b70ad888be610af9a5c50ed3119140b587a6c65
David Alderliesten <j.w.d.alderliesten@student.tudelft.nl>
2015-10-23 18:45:25 +0200

    Refactor tests
    Fixed the logger tests as per Jesse's feedback.
    
    <test

--- src/test/java/nl/tudelft/scrumbledore/LoggerTest.java
+++ src/test/java/nl/tudelft/scrumbledore/LoggerTest.java
@@ -3,13 +3,7 @@ package nl.tudelft.scrumbledore;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import org.junit.Before;
@@ -65,28 +59,15 @@ public class LoggerTest {
    * Test whether the desired data is written to disk when the Logger.log() method is being called.
    */
   @Test
+  @SuppressWarnings("PMD.AvoidDuplicateLiterals")
   public final void testLog() {
-    File loggingFile = logger.getLoggingFile();
     logger.log("log_write_test");
+    
+    ArrayList<String> fetchedLog = logger.getLines();
 
-    try {
-      BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(
-          loggingFile), "UTF-8"));
-
-      assertEquals("--------------------SCRUMBLEDORE LOGGING FILE", reader.readLine());
-      String lastLine = "";
-      String line;
-      while ((line = reader.readLine()) != null) {
-        lastLine = line;
-      }
-      assertEquals("log_write_test", lastLine);
-
-      reader.close();
-    } catch (UnsupportedEncodingException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    assertEquals("--------------------SCRUMBLEDORE LOGGING FILE", fetchedLog.get(0));
+    
+    assertEquals("log_write_test", fetchedLog.get(fetchedLog.size() - 1));
   }
 
   /**
