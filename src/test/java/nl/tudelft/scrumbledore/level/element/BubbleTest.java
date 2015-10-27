acb120c341c8eba000682d361ab7c030998a9343
NielsWarnars <Niels Warnars>
2015-10-27 18:51:26 +0100

    Extend Bubble test suite
    <organisation

--- src/test/java/nl/tudelft/scrumbledore/level/element/BubbleTest.java
+++ src/test/java/nl/tudelft/scrumbledore/level/element/BubbleTest.java
@@ -2,13 +2,17 @@ package nl.tudelft.scrumbledore.level.element;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import nl.tudelft.scrumbledore.Constants;
 import nl.tudelft.scrumbledore.level.Vector;
+import nl.tudelft.scrumbledore.sprite.Sprite;
 
 /**
  * Test Suite for the Bubble class.
@@ -82,5 +86,117 @@ public class BubbleTest extends LevelElementTest {
     test.addAction(LevelElementAction.MoveLeft);
     test.clearActions();
     assertFalse(test.hasAction(LevelElementAction.MoveLeft));
+  }
+  
+  /**
+   * Test the lastMove field getter/setter.
+   */
+  @Test
+  public void testLastMove() {
+    test.setLastMove(LevelElementAction.MoveLeft);
+    assertNull(test.getLastMove());
+  }
+  
+  /**
+   * Test the getActions method.
+   */
+  @Test
+  public void testGetActions() {
+    test.clearActions();
+    test.removeAction(LevelElementAction.MoveRight);
+    test.addAction(LevelElementAction.MoveLeft);
+    ArrayList<LevelElementAction> actions = test.getActions();
+    
+    assertEquals(1, actions.size());
+    assertEquals(LevelElementAction.MoveLeft, actions.get(0));
+  }
+  
+  /**
+   * Test the getter/setter methods of the lifetime attribute of a Bubble object.
+   */
+  @Test
+  public void testLifetime() {
+    test.setLifetime(42d);
+    assertEquals(42d, test.getLifetime(), Constants.DOUBLE_PRECISION);
+  }
+  
+  /**
+   * Test the getter/setter methods of the hasNPC attribute of a Bubble object.
+   */
+  @Test
+  public void testHasNPC() {
+    assertFalse(test.hasNPC());
+    test.setHasNPC(true);
+    assertTrue(test.hasNPC());
+  }
+  
+  /**
+   * Test the getSprites method to verify whether the correct
+   * sprite(s) is/are being returned.
+   */
+  @Test
+  public void testGetSprites() {
+    ArrayList<Sprite> sprites = test.getSprites(1);
+    
+    assertEquals(1, sprites.size());
+    assertEquals("frame-01", sprites.get(0).getID());
+    assertEquals("images\\sprites\\bubble-green/frame-01.png", sprites.get(0).getPath());
+  }
+  
+  /**
+   * Test the getSprites method to verify whether the correct  sprite(s) is/are being returned 
+   * if a bubble has a lifetime of less than 40 steps, but it still ends up red.
+   */
+  @Test
+  public void testGetSpritesRedBubble() {
+    test.setLifetime(7);
+    ArrayList<Sprite> sprites = test.getSprites(1);
+    
+    assertEquals(1, sprites.size());
+    assertEquals("frame-01", sprites.get(0).getID());
+    assertEquals("images\\sprites\\bubble-red/frame-01.png", sprites.get(0).getPath());
+  }
+  
+  /**
+   * Test the getSprites method to verify whether the correct  sprite(s) is/are being returned 
+   * if a bubble has a lifetime of less than 5 steps.
+   */
+  @Test
+  public void testGetSpritesGreenBurst() {
+    test.setLifetime(2);
+    ArrayList<Sprite> sprites = test.getSprites(1);
+    
+    assertEquals(1, sprites.size());
+    assertEquals("frame-01", sprites.get(0).getID());
+    assertEquals("images\\sprites\\bubble-green-burst/frame-01.png", sprites.get(0).getPath());
+  }
+  
+  /**
+   * Test the getSprites method to verify whether the correct
+   * sprite(s) is/are being returned if a bubble has an NPC.
+   */
+  @Test
+  public void testGetSpritesHasNPC() {
+    test.setHasNPC(true);
+    ArrayList<Sprite> sprites = test.getSprites(1);
+    
+    assertEquals(1, sprites.size());
+    assertEquals("frame-01", sprites.get(0).getID());
+    assertEquals("images\\sprites\\bubble-zenchan-green/frame-01.png", sprites.get(0).getPath());
+  }
+  
+  /**
+   * Test the getSprites method to verify whether the correct
+   * sprite(s) is/are being returned if a red bubble has an NPC.
+   */
+  @Test
+  public void testGetSpritesHasNPCRed() {
+    test.setHasNPC(true);
+    test.setLifetime(50);
+    ArrayList<Sprite> sprites = test.getSprites(1);
+    
+    assertEquals(1, sprites.size());
+    assertEquals("frame-01", sprites.get(0).getID());
+    assertEquals("images\\sprites\\bubble-zenchan-red/frame-01.png", sprites.get(0).getPath());
   }
 }
