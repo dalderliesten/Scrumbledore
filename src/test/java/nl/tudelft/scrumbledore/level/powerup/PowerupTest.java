f25514341fbc9ed6922f639fe4dc973ca0c84a49
Floris Doolaard <floris.do@gmail.com>
2015-10-30 14:03:36 +0100

    Added initial powerup testing class.

---
+++ src/test/java/nl/tudelft/scrumbledore/level/powerup/PowerupTest.java
@@ -0,0 +1,15 @@
+package nl.tudelft.scrumbledore.level.powerup;
+
+import nl.tudelft.scrumbledore.level.Vector;
+import nl.tudelft.scrumbledore.level.element.LevelElement;
+import nl.tudelft.scrumbledore.level.element.LevelElementTest;
+
+public class PowerupTest extends LevelElementTest{
+
+  @Override
+  public LevelElement make(Vector position, Vector size) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
