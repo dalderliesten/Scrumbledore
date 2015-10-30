0fb670bed2ebc00e31347caf05312c244175e1e4
Floris Doolaard <floris.do@gmail.com>
2015-10-30 20:17:47 +0100

    Finished the powerup pickup tests
    Reached about 90% coverage
    
    <testing

--- src/test/java/nl/tudelft/scrumbledore/level/powerup/PowerupPickUpTest.java
+++ src/test/java/nl/tudelft/scrumbledore/level/powerup/PowerupPickUpTest.java
@@ -1,5 +1,12 @@
 package nl.tudelft.scrumbledore.level.powerup;
 
-public class PowerupPickUpTest {
+import nl.tudelft.scrumbledore.level.Vector;
+import nl.tudelft.scrumbledore.level.element.LevelElement;
+import nl.tudelft.scrumbledore.level.element.LevelElementTest;
+
+public abstract class PowerupPickUpTest extends LevelElementTest {
+
+  @Override
+  public abstract LevelElement make(Vector position, Vector size);
 
 }
--- src/test/java/nl/tudelft/scrumbledore/level/powerup/TurtleTacoPickUpTest.java
+++ src/test/java/nl/tudelft/scrumbledore/level/powerup/TurtleTacoPickUpTest.java
@@ -1,5 +1,80 @@
 package nl.tudelft.scrumbledore.level.powerup;
 
-public class TurtleTacoPickUpTest{
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import nl.tudelft.scrumbledore.level.Vector;
+import nl.tudelft.scrumbledore.level.element.LevelElement;
+import nl.tudelft.scrumbledore.level.element.LevelElementAction;
+import nl.tudelft.scrumbledore.sprite.Sprite;
+
+/**
+ * Tests the TurtleTacoPickUp class which makes use of the LevelElementTest class.
+ * 
+ * @author Floris Doolaard
+ *
+ */
+public class TurtleTacoPickUpTest extends PowerupPickUpTest {
+
+  private TurtleTacoPickUp tT1;
+  private Vector position;
+  private Vector size;
+
+  /**
+   * Initializes the TurtleTacoPickUp object before testing.
+   */
+  @Before
+  public void setUp() {
+    position = new Vector(0, 0);
+    size = new Vector(0, 0);
+    tT1 = new TurtleTacoPickUp(position, size);
+  }
+
+  @Override
+  public LevelElement make(Vector position, Vector size) {
+    return new TurtleTacoPickUp(position, size);
+  }
+
+  /**
+   * Test the getSprites method to verify whether the correct sprite(s) is/are being returned.
+   */
+  @Test
+  public void testGetSprites() {
+    ArrayList<Sprite> sprites = tT1.getSprites(1);
+
+    assertEquals(1, sprites.size());
+    assertEquals("powerup-turtle-taco", sprites.get(0).getID());
+    assertEquals(
+        "images" + File.separator + "sprites" + File.separator + "powerup-turtle-taco.png", sprites
+            .get(0).getPath());
+  }
+
+  /**
+   * Tests if the actions of TurtleTacoPickUp are actually non-exsistent.
+   */
+  @Test
+  public void testActions() {
+    tT1.addAction(LevelElementAction.MoveLeft);
+    assertFalse(tT1.hasAction(LevelElementAction.MoveLeft));
+    tT1.removeAction(LevelElementAction.MoveLeft);
+    assertFalse(tT1.hasAction(LevelElementAction.MoveLeft));
+
+    assertEquals(null, tT1.getActions());
+  }
+
+  /**
+   * Tests whether TurtleTacoPickUp has really not moves.
+   */
+  @Test
+  public void testMoves() {
+    tT1.setLastMove(LevelElementAction.MoveLeft);
+    assertEquals(null, tT1.getLastMove());
+  }
 
 }
