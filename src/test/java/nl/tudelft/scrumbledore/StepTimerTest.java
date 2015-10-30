395186b03c4dd1b5b13f1ac9b75388b3d93a8e5a
David Alderliesten <j.w.d.alderliesten@student.tudelft.nl>
2015-10-30 18:07:59 +0100

    Implement StepTimer Tests
    Added essential basic steptimer tests that increased overall simple
    coverage.
    
    <feature, testing

--- src/test/java/nl/tudelft/scrumbledore/StepTimerTest.java
+++ src/test/java/nl/tudelft/scrumbledore/StepTimerTest.java
@@ -1,6 +1,7 @@
 package nl.tudelft.scrumbledore;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyDouble;
 import static org.mockito.Mockito.atLeast;
@@ -8,9 +9,7 @@ import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-
 import org.junit.Test;
-
 import nl.tudelft.scrumbledore.game.Game;
 import nl.tudelft.scrumbledore.game.MultiPlayerGame;
 import nl.tudelft.scrumbledore.game.SinglePlayerGame;
@@ -18,11 +17,94 @@ import nl.tudelft.scrumbledore.game.SinglePlayerGame;
 /**
  * Test Suite for the StepTimer class.
  * 
+ * @author David Alderliesten
  * @author Jesse Tilro
- *
  */
-@SuppressWarnings({ "PMD.JUnitTestsShouldIncludeAssert", "PMD.TooManyStaticImports" })
+@SuppressWarnings({ "PMD.JUnitTestsShouldIncludeAssert", "PMD.TooManyStaticImports",
+    "PMD.TooManyMethods" })
 public class StepTimerTest {
+
+  /**
+   * Testing the getting of the rate.
+   */
+  @Test
+  public void testGetRate() {
+    Game game = mock(Game.class);
+    StepTimer toTest = new StepTimer(30, game);
+
+    assertEquals(30, toTest.getRate());
+  }
+
+  /**
+   * Testing the setting of the rate, and verifying its function.
+   */
+  @Test
+  public void testSetRate() {
+    Game game = mock(Game.class);
+    StepTimer toTest = new StepTimer(30, game);
+
+    toTest.setRate(60);
+    assertEquals(60, toTest.getRate());
+  }
+
+  /**
+   * Testing the getting of the game to which the timer is connected/linked.
+   */
+  @Test
+  public void testGetGame() {
+    Game game = mock(Game.class);
+    StepTimer toTest = new StepTimer(30, game);
+
+    assertEquals(game, toTest.getGame());
+  }
+
+  /**
+   * Testing the setting of the game.
+   */
+  @Test
+  public void testSetGame() {
+    Game game = mock(Game.class);
+    StepTimer toTest = new StepTimer(30, game);
+
+    Game altGame = mock(Game.class);
+    toTest.setGame(altGame);
+    
+    assertEquals(altGame, toTest.getGame());
+  }
+  
+  /**
+   * Test the pausing of the game.
+   */
+  @Test
+  public void pauseGame() {
+    Game game = mock(Game.class);
+    StepTimer toTest = new StepTimer(30, game);
+    
+    assertFalse(toTest.isPaused());
+    
+    toTest.pause();
+    
+    assertTrue(toTest.isPaused());
+  }
+  
+  /**
+   * Test the resuming/restarting of the game.
+   */
+  @Test
+  public void resumeGame() {
+    Game game = mock(Game.class);
+    StepTimer toTest = new StepTimer(30, game);
+    
+    assertFalse(toTest.isPaused());
+    
+    toTest.pause();
+    
+    assertTrue(toTest.isPaused());
+    
+    toTest.resume();
+    
+    assertFalse(toTest.isPaused());
+  }
 
   /**
    * The game loop should aim to iterate at a steady rate. In this case it is tested by verifying
@@ -56,9 +138,9 @@ public class StepTimerTest {
     int rate = 30;
     int deviation = 2;
     Game game = new MultiPlayerGame();
-    // Initially set the wrong rate.
+
     StepTimer timer = new StepTimer(15, game);
-    // Set rate should update all internal parameters correctly.
+
     timer.setRate(rate);
 
     timer.start();
